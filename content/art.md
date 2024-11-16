2ca552846017f6a6748eef35ac233450747f4cfc
RVDEKID <rvdekid@gmail.com>
2024-11-16 19:36:35 +0200

    dynamic layout

--- content/art.md
+++ content/art.md
@@ -1,25 +1,6 @@
 ---
 title: "Art"
-layout: "art"
+layout: "index-flexible"
 url: "/art/"
 summary: art
 ---
-
-### Study and reference
-
-[[HTML Basics]] 
-[[CSS Basics]] 
-[[JavaScript Basics]] 
-
-
-### Creative 
-
-[[Visual WebDev Ideas]] 
-
-
-### Useful Links
-
-[Realtime Colors](https://realtimecolors.com/?colors=090608-f0eaed-454530-dbe6e0-646690)  Website for testing ui colors, accessibility, fonts. Super cool.
-
-### hugo
-https://reorx.github.io/hugo-PaperModX/docs/features/
---
+++ static/js/dyna-panels.js
@@ -0,0 +1,440 @@
+
+
+const content = {
+    home: {
+        left: `
+          <div class="box">
+            <div class="hero-left">
+            <h1 class="chromatic-aberration">RVDEKID</h1>
+            <p >Jimi Kampman, Finland</p>
+            </div>
+          </div>
+        `,
+        right: `
+          <div class="hero-right">
+            <div class="hero-right-container">
+            <div class="hero-image-container">
+            <img class="hero-image" src="/img/omakuva.jpg" alt="hero image">
+            </div>
+            <p class="hero-description">Graphic & web designer from Rovaniemi, Finland. <br> 
+            In my free time i paint stylized portraits & make games.</p>
+            </div>
+          </div>
+        `,
+        layout: 'layout-home',
+      },
+  art: {
+    left: `
+      <div class="box">
+        <h2>Gallery</h2>
+        <p>This is a gallery of my stylized portraits. <br>
+         Everything is handpainted digitally. I've been painting since
+         2021.</p>
+      </div>
+    `,
+    right: `
+    <div class="gallery">
+    <a href="/img/art/web/20241109.webp" class="gallery-item">
+    <img src="/img/art/web/20241109.webp" loading="lazy"/> 
+    </a>
+    <a href="/img/art/web/20240828.webp" class="gallery-item">
+    <img src="/img/art/web/20240828.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20240720.webp" class="gallery-item">
+    <img src="/img/art/web/20240720.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20240622.webp" class="gallery-item">
+    <img src="/img/art/web/20240622.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20240330.webp" class="gallery-item">
+    <img src="/img/art/web/20240330.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20231206.webp" class="gallery-item">
+    <img src="/img/art/web/20231206.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20231115.webp" class="gallery-item">
+    <img src="/img/art/web/20231115.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20231028.webp" class="gallery-item">
+    <img src="/img/art/web/20231028.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20231021.webp" class="gallery-item">
+    <img src="/img/art/web/20231021.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20230808.webp" class="gallery-item">
+    <img src="/img/art/web/20230808.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20230723.webp" class="gallery-item">
+    <img src="/img/art/web/20230723.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20230415.webp" class="gallery-item">
+    <img src="/img/art/web/20230415.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20230304.webp" class="gallery-item">
+    <img src="/img/art/web/20230304.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20230129.webp" class="gallery-item">
+    <img src="/img/art/web/20230129.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20230106.webp" class="gallery-item">
+    <img src="/img/art/web/20230106.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20221215.webp" class="gallery-item">
+    <img src="/img/art/web/20221215.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20220828.webp" class="gallery-item">
+    <img src="/img/art/web/20220828.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20220730.webp" class="gallery-item">
+    <img src="/img/art/web/20220730.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20220711.webp" class="gallery-item">
+    <img src="/img/art/web/20220711.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20220528.webp" class="gallery-item">
+    <img src="/img/art/web/20220528.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20220430.webp" class="gallery-item">
+    <img src="/img/art/web/20220430.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20220427.webp" class="gallery-item">
+    <img src="/img/art/web/20220427.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20220219.webp" class="gallery-item">
+    <img src="/img/art/web/20220219.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20220129.webp" class="gallery-item">
+    <img src="/img/art/web/20220129.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20220125.webp" class="gallery-item">
+    <img src="/img/art/web/20220125.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20220113.webp" class="gallery-item">
+    <img src="/img/art/web/20220113.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20211031.webp" class="gallery-item">
+    <img src="/img/art/web/20211031.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20211021.webp" class="gallery-item">
+    <img src="/img/art/web/20211021.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20211011.webp" class="gallery-item">
+    <img src="/img/art/web/20211011.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20211006.webp" class="gallery-item">
+    <img src="/img/art/web/20211006.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20211004.webp" class="gallery-item">
+    <img src="/img/art/web/20211004.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20211003.webp" class="gallery-item">
+    <img src="/img/art/web/20211003.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210929.webp" class="gallery-item">
+    <img src="/img/art/web/20210929.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210927.webp" class="gallery-item">
+    <img src="/img/art/web/20210927.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210917.webp" class="gallery-item">
+    <img src="/img/art/web/20210917.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210911.webp" class="gallery-item">
+    <img src="/img/art/web/20210911.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210829.webp" class="gallery-item">
+    <img src="/img/art/web/20210829.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210828.webp" class="gallery-item">
+    <img src="/img/art/web/20210828.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210827.webp" class="gallery-item">
+    <img src="/img/art/web/20210827.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210819.webp" class="gallery-item">
+    <img src="/img/art/web/20210819.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210817.webp" class="gallery-item">
+    <img src="/img/art/web/20210817.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210811.webp" class="gallery-item">
+    <img src="/img/art/web/20210811.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210802.webp" class="gallery-item">
+    <img src="/img/art/web/20210802.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210729.webp" class="gallery-item">
+    <img src="/img/art/web/20210729.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210728.webp" class="gallery-item">
+    <img src="/img/art/web/20210728.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210723.webp" class="gallery-item">
+    <img src="/img/art/web/20210723.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210709.webp" class="gallery-item">
+    <img src="/img/art/web/20210709.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210707.webp" class="gallery-item">
+    <img src="/img/art/web/20210707.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210701.webp" class="gallery-item">
+    <img src="/img/art/web/20210701.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210630.webp" class="gallery-item">
+    <img src="/img/art/web/20210630.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210629.webp" class="gallery-item">
+    <img src="/img/art/web/20210629.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210622.webp" class="gallery-item">
+    <img src="/img/art/web/20210622.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210531.webp" class="gallery-item">
+    <img src="/img/art/web/20210531.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210529.webp" class="gallery-item">
+    <img src="/img/art/web/20210529.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210527.webp" class="gallery-item">
+    <img src="/img/art/web/20210527.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210526.webp" class="gallery-item">
+    <img src="/img/art/web/20210526.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210525.webp" class="gallery-item">
+    <img src="/img/art/web/20210525.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210524.webp" class="gallery-item">
+    <img src="/img/art/web/20210524.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210523.webp" class="gallery-item">
+    <img src="/img/art/web/20210523.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210521.webp" class="gallery-item">
+    <img src="/img/art/web/20210521.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210519.webp" class="gallery-item">
+    <img src="/img/art/web/20210519.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210518.webp" class="gallery-item">
+    <img src="/img/art/web/20210518.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210517.webp" class="gallery-item">
+    <img src="/img/art/web/20210517.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210515.webp" class="gallery-item">
+    <img src="/img/art/web/20210515.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210514.webp" class="gallery-item">
+    <img src="/img/art/web/20210514.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210512.webp" class="gallery-item">
+    <img src="/img/art/web/20210512.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210505.webp" class="gallery-item">
+    <img src="/img/art/web/20210505.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210503.webp" class="gallery-item">
+    <img src="/img/art/web/20210503.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210502.webp" class="gallery-item">
+    <img src="/img/art/web/20210502.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210501.webp" class="gallery-item">
+    <img src="/img/art/web/20210501.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210430.webp" class="gallery-item">
+    <img src="/img/art/web/20210430.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210429.webp" class="gallery-item">
+    <img src="/img/art/web/20210429.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210428.webp" class="gallery-item">
+    <img src="/img/art/web/20210428.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210427.webp" class="gallery-item">
+    <img src="/img/art/web/20210427.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210426.webp" class="gallery-item">
+    <img src="/img/art/web/20210426.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210425.webp" class="gallery-item">
+    <img src="/img/art/web/20210425.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210424.webp" class="gallery-item">
+    <img src="/img/art/web/20210424.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210423.webp" class="gallery-item">
+    <img src="/img/art/web/20210423.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210422.webp" class="gallery-item">
+    <img src="/img/art/web/20210422.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210421.webp" class="gallery-item">
+    <img src="/img/art/web/20210421.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210418.webp" class="gallery-item">
+    <img src="/img/art/web/20210418.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210417.webp" class="gallery-item">
+    <img src="/img/art/web/20210417.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210416.webp" class="gallery-item">
+    <img src="/img/art/web/20210416.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210413.webp" class="gallery-item">
+    <img src="/img/art/web/20210413.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210411.webp" class="gallery-item">
+    <img src="/img/art/web/20210411.webp" loading="lazy"/>
+    </a>
+    <a href="/img/art/web/20210409.webp" class="gallery-item">
+    <img src="/img/art/web/20210409.webp" loading="lazy"/>
+    </a>
+    <script>
+    // Initialize Masonry
+    var elem = document.querySelector('.gallery');
+    var msnry = new Masonry(elem, {
+      itemSelector: '.gallery-item',
+      columnWidth: '.gallery-item',
+      percentPosition: true,
+      gutter: 20,
+    });
+
+    // Initialize SimpleLightbox
+    var lightbox = new SimpleLightbox('.gallery a', {
+      captions: true,
+      captionSelector: 'img',
+      captionType: 'attr',
+      overlay: true,
+    });
+  </script>
+    </div>
+
+
+    `,
+    layout: 'layout-art',
+  },
+  portfolio: {
+    left: `
+      <h2> Here i will have my project portfolio</h2>
+      <p>This side will have filters for different types of work.</p>
+    `,
+    right: `
+      <p>This will be a list of different projects you can click open and read more about.</p>
+      <ul class="list">
+        <li>List Item 1</li>
+        <li>List Item 2</li>
+        <li>List Item 3</li>
+      </ul>
+    `,
+    layout: 'layout-portfolio',
+  },
+  cv: {
+    left: `
+      <h2> This will be my resume</h2>
+      <p>I'll copy my work experience here.</p>
+    `,
+    right: `
+      <h3> Skills </h3>
+        <ul class="list">
+        <li>skill 1</li>
+        <li>will add skill bars</li>
+        <li>to show comfortability</li>
+      </ul>
+      <h3> Hobbies </3>
+      <ul class="list">
+        <li>hobby1</li>
+        <li>hobbies that are relevant</li>
+        <li>and showcase my vibe</li>
+      </ul>
+    `,
+    layout: 'layout-cv',
+  },
+};
+
+
+const leftPanel = document.getElementById('left-panel');
+const rightPanel = document.getElementById('right-panel');
+
+function loadGalleryContent() {
+    // Your logic for dynamically loading gallery content
+  
+    // Ensure the gallery element exists before initializing Masonry
+    var elem = document.querySelector('.gallery');
+    
+    if (elem) {
+      // Start polling every 0.1s for 10 times
+    let count = 0;
+    const interval = setInterval(function() {
+    var msnry = new Masonry('.gallery', {
+    itemSelector: '.gallery-item',
+    columnWidth: '.gallery-item',
+    percentPosition: true,
+    gutter: 10,
+  });
+  
+  // Stop the loop after 10 iterations
+  count++;
+  if (count >= 5) {
+    clearInterval(interval);
+  }
+}, 100); // Run every 100ms (0.1s)
+  
+      // Reinitialize SimpleLightbox for new images
+      var lightbox = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionSelector: 'img',
+        captionType: 'attr',
+        overlay: true,
+      });
+      
+      // After new items have been added to the gallery, re-layout Masonry
+      msnry.reloadItems(); // Reload the layout with new items
+      msnry.layout(); // Layout the items properly
+    } else {
+      console.error('Gallery element not found.');
+    }
+  }
+  
+  // Call this function after your gallery content is loaded dynamically
+ 
+  
+  
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.classList.add('layout-home'); // Set the default layout on page load
+    navigateTo('home'); // Automatically populate the content with the 'home' page on load
+});
+
+document.querySelectorAll('button[data-action]').forEach((button) => {
+    button.addEventListener('click', () => {
+        const action = button.getAttribute('data-action');
+        const param = button.getAttribute('data-param');
+        window[action](param);
+    });
+});
+
+
+
+function navigateTo(page) {
+  const pageData = content[page];
+
+  setTimeout(() => {
+    // Update inner HTML of the panels
+    leftPanel.innerHTML = pageData.left;
+    rightPanel.innerHTML = pageData.right;
+
+    // Remove previous layout class if exists
+    const existingLayoutClass = Object.values(content).map(data => data.layout);
+    existingLayoutClass.forEach(layout => {
+      document.body.classList.remove(layout);
+    });
+
+    const currentLayout = document.body.classList.contains(pageData.layout);
+    document.body.classList.add(pageData.layout);
+    applyChromaticAberration(); 
+    loadGalleryContent();
+  }, 300); // Adjust timing for the fade-out effect
+}
